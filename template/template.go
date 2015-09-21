216de3b3684d59a62c603d6e38aff2000a413048
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-09-22 00:27:35 +0800

    Added template module to template pkg

---
+++ template/template.go
@@ -0,0 +1,40 @@
+package template
+
+import "html/template"
+
+var HelperFuncs []template.FuncMap
+
+func init() {
+	HelperFuncs = []template.FuncMap{
+		{
+			"toAge":              ToAge,
+			"safeHTML":           SafeHTML,
+			"escapeHTML":         EscapeHTML,
+			"sanitizeHTML":       SanitizeHTML,
+			"markdownHTML":       MarkdownHTML,
+			"nl2br":              Nl2br,
+			"toList":             List,
+			"timeSince":          TimeSince,
+			"diffLineTypeToStr":  DiffLineTypeToStr,
+			"diffTypeToStr":      DiffTypeToStr,
+			"sha1":               EncodeSha1,
+			"toUnix":             ToUnix,
+			"truncate":           Truncate,
+			"toDay":              ToDay,
+			"toMonth":            ToMonth,
+			"toYear":             ToYear,
+			"datenow":            Datenow,
+			"shuffle":            Shuffle,
+			"reverse":            Reverse,
+			"slice":              Slice,
+			"plus":               Plus,
+			"autoLink":           AutoLink,
+			"extractIMGs":        ExtractIMGs,
+			"genMetaTitle":       GenMetaTitle,
+			"genMetaKeywords":    GenMetaKeywords,
+			"genMetaDescription": GenMetaDescription,
+			"splitFoldl":         SplitFoldl,
+			"splitFoldr":         SplitFoldr,
+		},
+	}
+}
